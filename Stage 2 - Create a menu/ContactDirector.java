67ac889fc3bc8df72a46d52351bc922b6cb7fb3a
Combat47 <115223808+combat47@users.noreply.github.com>
2023-11-04 13:26:15 +0330

    ContactDirector.java

---
+++ Stage 2 - Create a menu/ContactDirector.java
@@ -0,0 +1,16 @@
+package contacts;
+
+import java.util.Scanner;
+
+public class ContactDirector {
+    Scanner scanner = new Scanner(System.in);
+
+    public void buildContact(Builder builder) {
+        System.out.print("Enter the name: ");
+        builder.setName(scanner.nextLine());
+        System.out.print("Enter the surname: ");
+        builder.setSurname(scanner.nextLine());
+        System.out.print("Enter the number: ");
+        builder.setNumber(scanner.nextLine());
+    }
+}
