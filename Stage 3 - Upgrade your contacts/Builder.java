fb4f11b1a5021311340f05f90976c428e529eb02
Combat47 <115223808+combat47@users.noreply.github.com>
2023-11-05 10:57:01 +0330

    Builder.java

---
+++ Stage 3 - Upgrade your contacts/Builder.java
@@ -0,0 +1,13 @@
+package contacts;
+
+interface Builder {
+    void setName(String name);
+    void setSurname(String surname);
+    void setNumber(String number);
+    void setOrganizationName(String organizationName);
+    void setAddress(String address);
+    void setBirthDay(String birthDay);
+    void setGender(String gender);
+    Contact getPerson();
+    Contact getOrganization();
+}
